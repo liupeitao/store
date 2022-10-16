809e57cda752aecda994c2e65d049e560a192907
lpt <sudolovelnpctly6869@outlook.com>
2022-10-17 01:34:19 +0800

    09 添加了， 拦截器， 前一个提交好像出现了问题，这次是为了保证正确。

--- src/main/java/com/example/demo/config/LoginInterceptorConfig.java
+++ src/main/java/com/example/demo/config/LoginInterceptorConfig.java
@@ -1,2 +1,36 @@
-package com.example.demo.config;public class LoginInterceptorConfig {
+package com.example.demo.config;
+
+import com.example.demo.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+// 加载当前拦截器配置
+@Configuration
+public class LoginInterceptorConfig implements WebMvcConfigurer {
+    //自定义拦截器对象
+    private HandlerInterceptor handlerInterceptor = new LoginInterceptor();
+    private List<String> patterns = new ArrayList<>();
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        setPatterns();
+        registry.addInterceptor(handlerInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(patterns);
+    }
+
+    public  void setPatterns(){
+        patterns.add("/bootstrap3/**");
+        //patterns.add("/css/**");
+        patterns.add("/web/index.html");
+        patterns.add("/web/login.html");
+        patterns.add("web/register.html");
+        patterns.add("/users/reg");
+        patterns.add("/users/login");
+    }
 }
--- src/main/java/com/example/demo/interceptor/LoginInterceptor.java
+++ src/main/java/com/example/demo/interceptor/LoginInterceptor.java
@@ -1,2 +1,38 @@
-package com.example.demo.interceptor;public class LoginInterceptor {
+package com.example.demo.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    /**
+     * 因为我们在login的时候才把uid 射入进session.所以get不到uid就说明没有登录。
+     *检测全局的Session对象中是否有uid数据， 如果有则放行， 没有就重定向到 login.
+     * @param request 请求对象
+     * @param response 响应对象
+     * @param handler  处理器（url + cotroller 映射）
+     * @return true放行  false则拦截当前请求、
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object obj = request.getSession().getAttribute("uid").toString();
+        if(obj == null){
+            response.sendRedirect("/web/login.html");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
 }
